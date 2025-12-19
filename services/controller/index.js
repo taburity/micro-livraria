08f5c2e42d9933e5b73bd5f1a967be2f656bd731
memelon220 <luigimateo220@gmail.com>
2025-12-18 22:30:05 -0300

    Tarefa prática #2 - Docker

--- services/controller/index.js
+++ services/controller/index.js
@@ -41,7 +41,6 @@ app.get('/shipping/:cep', (req, res, next) => {
         }
     );
 });
-
 app.get('/product/:id', (req, res, next) => {
     inventory.SearchProductByID({ id: req.params.id }, (err, product) => {
         if (err) {
@@ -52,7 +51,6 @@ app.get('/product/:id', (req, res, next) => {
         }
     });
 });
-
 /**
  * Inicia o router
  */
